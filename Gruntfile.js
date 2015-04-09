87657957df3106bb5f3939e215635258e9625f7b
nase00 <nase00@gmail.com>
2015-04-08 19:30:42 -0500

    Create skeleton.

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+ 
+  grunt.initConfig({
+    connect: {
+      all: {
+        options:{
+          port: 9000, // keepalive server is 8000
+          hostname: "localhost",
+          keepalive: true
+        },
+      },
+      keepalive: true,
+    },
+    browserify: {
+      options: {
+        transform: [ require("grunt-react").browserify ]
+      },
+      app: {
+        src: "build/jsx/application.jsx",
+        dest: "assets/application.js"
+      }
+    },
+    less: {
+      development: {
+         options: {
+             paths: ["assets"]
+         },
+         files: {"assets/application.css": "build/less/application.less"}
+     },
+    },
+    uglify: {
+      my_target: {
+        files: {
+          "assets/js/application.min.js": "assets/js/application.js"
+        }
+      }
+    },
+    watch: {
+      files: ["./build/**/*"],
+      tasks: ["browserify", "less"],
+    },
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-react");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+
+  grunt.event.on("watch", function(action, filepath, target) {
+    grunt.log.writeln(target + ": " + filepath + " has " + action);
+  });
+
+  grunt.registerTask("server",[
+    "connect:keepalive",
+    "watch"
+  ]);
+};
