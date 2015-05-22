d01a89e9cba36386388a47f2c445030fa316e826
Nase00 <nase00@gmail.com>
2015-05-22 12:30:50 -0500

    Reformat styling to be human-readable.

--- assets/application.js
+++ assets/application.js
@@ -29283,7 +29283,45 @@ var Chicago = new google.maps.LatLng(41.866867, -87.607076)
     , app = require('../application.jsx')
 
 mapStyle = [
-    {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":55}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}
+    {
+      "featureType":"administrative",
+      "elementType":"labels.text.fill",
+      "stylers":[{"color":"#444444"}]
+    },
+    {
+      "featureType":"landscape",
+      "elementType":"all",
+      "stylers":[{"color":"#f2f2f2"}]
+    },
+    {
+      "featureType":"poi",
+      "elementType":"all",
+      "stylers":[{"visibility":"off"}]
+    },
+    {
+      "featureType":"road",
+      "elementType":"all",
+      "stylers":[{"saturation":-100},{"lightness":55}]
+    },
+    {
+      "featureType":"road.highway",
+      "elementType":"all","stylers":[{"visibility":"simplified"}]
+    },
+    {
+      "featureType":"road.arterial",
+      "elementType":"labels.icon",
+      "stylers":[{"visibility":"off"}]
+    },
+    {
+      "featureType":"transit",
+      "elementType":"all",
+      "stylers":[{"visibility":"off"}]
+    },
+    {
+      "featureType":"water",
+      "elementType":"all",
+      "stylers":[{"color":"#46bcec"},{"visibility":"on"}]
+    }
   ]
 
 var mapOptions = {
@@ -29333,6 +29371,7 @@ module.exports = {
   , streetView: streetViewOptions
   , render: rendererOptions 
 }
+
 },{"../application.jsx":207}],213:[function(require,module,exports){
 var app = require('./application.jsx')
 
